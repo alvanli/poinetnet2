3b44e5b4dfa23f20d6e7509d31291ecdc3c0e87a
Alvin <alvin.ipad2@gmail.com>
2021-08-21 00:32:17 -0400

    make feature dim 3

--- PN2_931/pointnet2/models/pointnet2_ssg_cls.py
+++ PN2_931/pointnet2/models/pointnet2_ssg_cls.py
@@ -99,7 +99,7 @@ class PointNet2ClassificationSSG(pl.LightningModule):
 
     def _break_up_pc(self, pc):
         xyz = pc[..., 0:3].contiguous()
-        features = pc[..., 3:].transpose(1, 2).contiguous() if pc.size(-1) > 3 else None
+        features = pc[..., 3:6].transpose(1, 2).contiguous() if pc.size(-1) > 3 else None
 
         return xyz, features
 
