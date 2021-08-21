e52a0619334e6f21ceea81c8a1032154f8157262
Alvin <alvin.ipad2@gmail.com>
2021-08-20 22:16:15 -0400

    unzip only

--- PN2_931/pointnet2/data/Indoor3DSemSegLoader.py
+++ PN2_931/pointnet2/data/Indoor3DSemSegLoader.py
@@ -34,9 +34,9 @@ class Indoor3DSemSeg(data.Dataset):
 
         if download and not os.path.exists(self.data_dir):
             zipfile = os.path.join(BASE_DIR, os.path.basename(self.url))
-            subprocess.check_call(
-                shlex.split("wget {} --no-check-certificate".format(self.url))
-            )
+            # subprocess.check_call(
+            #     shlex.split("wget {} --no-check-certificate".format(self.url))
+            # )
 
             subprocess.check_call(
                 shlex.split("unzip {} -d {}".format(zipfile, BASE_DIR))
